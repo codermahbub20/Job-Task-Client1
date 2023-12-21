31582d62a13eb5b7f126018c1aa16ae59c171e1c
codermahbub20 <mkrefat5@gmail.com>
2023-12-21 20:12:07 +0600

    Navbar are implemented

---
+++ src/Components/Pages/Banner/Banner.jsx
@@ -0,0 +1,20 @@
+
+
+const Banner = () => {
+    return (
+        <div>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <img src="https://i0.wp.com/getflowdotcom.wpcomstaging.com/wp-content/uploads/2020/05/task-management-tips.jpg?fit=1255%2C835&ssl=1" className="max-w-xl rounded-lg shadow-2xl" />
+                    <div>
+                        <h1 className="text-5xl font-bold">Box Office News!</h1>
+                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        <button className="btn btn-primary">Get Started</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
